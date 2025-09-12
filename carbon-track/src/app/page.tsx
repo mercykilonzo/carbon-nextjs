986c2c3dca94f28e14c98b9dfda25578198c10f5
Bonnette <bonnetteumu@gmail.com>
2025-09-12 22:06:20 +0300

    added new changes

--- carbon-track/src/app/page.tsx
+++ carbon-track/src/app/page.tsx
@@ -1,9 +1,11 @@
 
 import MqttSubscriber from './hivemq/mqtt_client'
+import Sidebar from './sharedComponents/SideBar';
 export default function Home() {
   return (
     <div>
       <h1>HiveMQ MQTT Data Subscriber</h1>
+      <Sidebar/>
       <MqttSubscriber />
     </div>
   );
