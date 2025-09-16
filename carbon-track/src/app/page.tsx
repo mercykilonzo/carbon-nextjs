5ed222f548ffceb18c0d650eafedef8b36401248
Mwikali Mercy <mwikalimercykilonzo@gmail.com>
2025-09-16 11:56:13 +0300

    Update page.tsx

--- carbon-track/src/app/page.tsx
+++ carbon-track/src/app/page.tsx
@@ -3,11 +3,6 @@ import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 export default function HomeRedirect() {
-  const router = useRouter();
-  useEffect(() => {
-    router.replace("/records");
-  }, [router]);
-  return null;
-
+ 
   <MqttSubscriber />
 }
